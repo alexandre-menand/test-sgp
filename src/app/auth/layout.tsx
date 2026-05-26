26f26df956160827ebcd59dfb3f68d1c4a55d3d7
Alexandre Menand <alexandre.menand.pro@gmail.com>
2026-05-26 19:03:14 +0200

    Feat: Style du Layout auth

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,22 @@
+import styles from "./layout.module.css"
+import Image from "next/image";
+import Illustration from "@/../public/illustration.svg";
+import Shape from "@/../public/shape.svg";
+
 export default function AuthLayout ({children}: {children : React.ReactNode}) {
     return (
-        <main>
-            test layout
-            {children}
+        <main className={styles.main}>
+            <div className={styles.containerForm}>
+                <h1 className={styles.title}>Les ateliers du Baobab</h1>
+                {children}
+            </div>
+            <div className={styles.containerImg}>
+                <Image src={Illustration} alt="" />
+            </div>
+
+            <div className={styles.containerShape}>
+                <Image src={Shape} alt="" className={styles.shape}/>
+            </div>
         </main>
     )
 }
