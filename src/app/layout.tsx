43affdf94cb749987d4146e38af662613b572f86
Alexandre Menand <alexandre.menand.pro@gmail.com>
2026-05-26 19:04:21 +0200

    Feat: Ajout de StyledEngineProvider pour ajouter des styles personnalisés aux composants MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
+import { StyledEngineProvider } from "@mui/material";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`}>
       <AppRouterCacheProvider>
-        <body>{children}</body>
+        <StyledEngineProvider injectFirst>
+          <body>{children}</body>
+        </StyledEngineProvider>
       </AppRouterCacheProvider>
     </html>
   );
