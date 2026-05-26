02a109eada46e9e64be9988955100b2c9735b2cd
Alexandre Menand <alexandre.menand.pro@gmail.com>
2026-05-26 20:11:34 +0200

    Chore: ajout d'un commentaire pour le redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@
 import {redirect} from "next/navigation";
 
 export default function Home() {
+    /**
+     * FIXME: création d'une redirection pour envoyer vers la page de login
+     */
   redirect("/auth/login");
   return (
     <div>
